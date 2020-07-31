a5af38bed357a06cd3d39e3894ee9743bd914250
Kayla Melton <kannmelton2@gmail.com>
2020-07-30 17:17:15 -0700

    Add project files.

---
+++ LetterLoops/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Linq;
+
+namespace LetterLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Give me some letters");
+
+            var readThis = Console.ReadLine();
+
+            char[] lettersArr = readThis.ToCharArray();
+
+            var finalCharList = lettersArr.Select(c => c.ToString()).ToArray();
+
+            for (int i = 0; i < lettersArr.Length; i++)
+            {
+                var singleChar = string.Join("", Enumerable.Repeat(lettersArr[i], i+1));
+                finalCharList[i] = UpperCaseFirst(singleChar);
+            }
+
+            static string UpperCaseFirst(string s)
+            {
+                if (string.IsNullOrEmpty(s))
+                {
+                    return string.Empty;
+                }
+
+                char[] a = s.ToCharArray();
+                a[0] = char.ToUpper(a[0]);
+                return new string(a);
+            }
+
+            Console.WriteLine(string.Join('-', finalCharList));
+        }
+    }
+}
